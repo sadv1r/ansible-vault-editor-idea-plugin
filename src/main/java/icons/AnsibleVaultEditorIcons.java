afa5dbbdfa62cd989730fe14bed5681870117579
Dmitry <sadv1r@users.noreply.github.com>
2020-05-15 00:22:28 +0300

    Added ansible icon on tool action

---
+++ src/main/java/icons/AnsibleVaultEditorIcons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface AnsibleVaultEditorIcons {
+    Icon ANSIBLE_ICON = IconLoader.getIcon("/icons/ansible.svg");
+}
