48272c7f7cc3b199226ebc94b20aa2fffc79671e
sadv1r <sadv12@gmail.com>
2020-06-06 05:08:18 +0300

    #36 Added possibility to decrypt by Alt+Enter

---
+++ src/main/kotlin/ru/sadv1r/idea/plugin/ansible/vault/editor/VaultModifyIntentionAction.kt
@@ -0,0 +1,56 @@
+package ru.sadv1r.idea.plugin.ansible.vault.editor
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import ru.sadv1r.ansible.vault.VaultHandler
+import ru.sadv1r.ansible.vault.crypto.VaultInfo
+import ru.sadv1r.idea.plugin.ansible.vault.editor.ui.VaultEditorDialog
+import ru.sadv1r.idea.plugin.ansible.vault.editor.ui.VaultPasswordDialog
+import java.io.IOException
+
+class VaultModifyIntentionAction : PsiElementBaseIntentionAction(), IntentionAction {
+
+    override fun getText(): String {
+        return "Modify Vault"
+    }
+
+    override fun getFamilyName(): String {
+        return "Ansible Vault"
+    }
+
+    override fun startInWriteAction(): Boolean {
+        return false
+    }
+
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
+        if (editor == null) {
+            return false
+        }
+
+        return editor.document.text.startsWith(VaultInfo.MAGIC_PART_DATA)
+    }
+
+    override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+        val document = editor?.document ?: return
+
+        val password = getPassword(document)
+        if (password == null) {
+            VaultPasswordDialog(document).showAndGet()
+        } else {
+            try {
+                VaultEditorDialog(
+                    document,
+                    VaultHandler.decrypt(document.text, password),
+                    password.toCharArray()
+                ).showAndGet()
+            } catch (e: IOException) {
+                removePassword(document)
+                VaultPasswordDialog(document).showAndGet()
+            }
+        }
+    }
+
+}
