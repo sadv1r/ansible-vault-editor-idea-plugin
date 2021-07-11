5ffb75fd83d9c59c52a5bd49fa1d848a9c9a3fc8
sadv1r <sadv12@gmail.com>
2021-07-11 03:48:49 +0300

    Refactoring

--- src/main/kotlin/ru/sadv1r/idea/plugin/ansible/vault/editor/action/OpenEditorAction.kt
+++ src/main/kotlin/ru/sadv1r/idea/plugin/ansible/vault/editor/action/OpenEditorAction.kt
@@ -5,11 +5,6 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import ru.sadv1r.ansible.vault.crypto.VaultInfo
 import ru.sadv1r.idea.plugin.ansible.vault.editor.FileVault
-import ru.sadv1r.idea.plugin.ansible.vault.editor.getPassword
-import ru.sadv1r.idea.plugin.ansible.vault.editor.removePassword
-import ru.sadv1r.idea.plugin.ansible.vault.editor.ui.VaultEditorDialog
-import ru.sadv1r.idea.plugin.ansible.vault.editor.ui.VaultPasswordDialog
-import java.io.IOException
 
 class OpenEditorAction : AnAction() {
 
@@ -24,21 +19,7 @@ class OpenEditorAction : AnAction() {
         val document = e.getData(PlatformDataKeys.EDITOR)?.document ?: return
         val vault = FileVault(document)
 
-        val password = getPassword(vault)
-        if (password == null) {
-            VaultPasswordDialog(vault).showAndGet()
-        } else {
-            try {
-                VaultEditorDialog(
-                    vault.getDecryptedData(password),
-                    password.toCharArray(),
-                    vault
-                ).showAndGet()
-            } catch (e: IOException) {
-                removePassword(vault)
-                VaultPasswordDialog(vault).showAndGet()
-            }
-        }
+        vault.openEditor()
     }
 
 }
